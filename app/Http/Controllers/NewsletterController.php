5340421df449e34a916be42c924098b801eaca5c
colonca1999@gmail.com <camilo14>
2020-08-14 10:11:46 -0500

    landing page

---
+++ app/Http/Controllers/NewsletterController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Newsletter;
+use Illuminate\Http\Request;
+
+class NewsletterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // dd($request->all());
+        $exist = Newsletter::where('email',$request->email)->first();
+        if(!$exist){
+            $newsletter = new Newsletter($request->all());
+            $result = $newsletter->save();
+            if ($result) {
+                return response()->json([
+                    'response' => "Has sido suscrito de manera exitosa",
+                    'status' => 'ok'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 'error'
+                ]);
+            }
+        }else {
+            return response()->json([
+                'response' => "Te encuentras suscrito a nuestro newsletter, Gracias por intentarlo nuevamente",
+                'status' => 'ok'
+            ]);
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Newsletter $newsletter
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Newsletter $newsletter)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param \App\Newsletter $newsletter
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Newsletter $newsletter)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Newsletter $newsletter
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Newsletter $newsletter)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Newsletter $newsletter
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Newsletter $newsletter)
+    {
+        //
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,275 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Appsiel</title>
+    <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@400;700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/33bf1ad68d.js" crossorigin="anonymous"></script>
+    <link href="{{asset('css/main.css')}}" rel="stylesheet">
+</head>
+<body>
+<header class="header">
+    <div class="wraper"><img class="headervideos__logo" src="./images/Lg_ 1.png" alt="appsiel">
+        <label class="menu-open" for="open">
+            <div class="menu__barra"></div>
+            <div class="menu__barra"></div>
+            <div class="menu__barra"></div>
+        </label>
+        <input id="open" type="checkbox">
+        <nav class="menu">
+            <ul class="nav">
+                <li class="nav__item"><a class="nav__link" href="">servicios<i class="fas fa-sort-down"></i></a>
+                    <ul class="nav__submenu">
+                        <li class="submenu__item"><a class="nav__link" href="">Diseño Web <i
+                                    class="fas fa-sort-down"></i></a>
+                            <ul class="nav__submenu">
+                                <li class="submenu__item"><a class="nav__link" href="">Diseño Web</a></li>
+                                <li class="submenu__item"><a class="nav__link" href="">Desarrollo de apps</a></li>
+                                <li class="submenu__item"><a class="nav__link" href="">Desarrollo Web</a></li>
+                                <li class="submenu__item"><a class="nav__link" href="">Gestion Educativa</a></li>
+                                <li class="submenu__item"><a class="nav__link" href="">Gestion Empresarial</a></li>
+                                <li class="submenu__item"><a class="nav__link" href="">Gestion Salud</a></li>
+                            </ul>
+                        </li>
+                        <li class="submenu__item"><a class="nav__link" href="">Desarrollo de apps</a></li>
+                        <li class="submenu__item"><a class="nav__link" href="">Desarrollo Web</a></li>
+                        <li class="submenu__item"><a class="nav__link" href="">Gestion Educativa</a></li>
+                        <li class="submenu__item"><a class="nav__link" href="">Gestion Empresarial</a></li>
+                        <li class="submenu__item"><a class="nav__link" href="">Gestion Salud</a></li>
+                    </ul>
+                </li>
+                <li class="nav__item"><a class="nav__link" href="">tecnologías <i class="fas fa-sort-down"></i></a></li>
+                <li class="nav__item"><a class="nav__link" href="">portafolio <i class="fas fa-sort-down"></i></a></li>
+                <li class="nav__item"><a class="nav__link" href="">blog</a></li>
+                <li class="nav__item"><a class="nav__link activo" href="">demo</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+<section class="hero">
+    <div class="wraper"><img class="hero__background" src="./svg/background.svg" alt="">
+        <div class="hero__content">
+            <div class="hero__description"><h1 class="hero__title">sistemas para el crecimiento empresarial</h1>
+                <p class="hero__text">Somos una empresa dedicada al desarrollo de sistemas de información que aumenten
+                    la productividad de nuestros clientes.</p></div>
+            <img class="hero__ilustracion" src="./svg/ilustracion.svg" alt=""></div>
+    </div>
+</section>
+<main class="servicios">
+    <div class="wraper"><h2>Servicios de software en la nube</h2></div>
+    <div class="background__grid">
+        <div class="servicios__grid">
+            <div class="servicios__item"><img class="logo" src="./images/clip-online-education-4.png" alt="Diseño web">
+                <h3 class="title">Diseño web</h3>
+                <p class="descricion">Empresa especializada en diseño de páginas web profesionales. Agencia de diseño y
+                    desarrollo de sitios web</p></div>
+            <div class="servicios__item"><img class="logo" src="./images/pale-computer-technology 1.png"
+                                              alt="Diseño web">
+                <h3 class="title">Desarrollo Web</h3>
+                <p class="descricion">Presencia online impactante para su negocio. Construya la columna vertebral de su
+                    estrategia de marketing digital.Haga su negocio visible a todo el mundo</p></div>
+            <div class="servicios__item"><img class="logo" src="./images/clip-online-education-4 1.png"
+                                              alt="Diseño web">
+                <h3 class="title">Consultoría en gestión empresarial</h3>
+                <p class="descricion">Todos los conocimientos, habilidades y experiencia de un equipo de profesionales
+                    al servicio de su negocio. Análisis financiero y diseño de plan estratégico.</p></div>
+            <div class="servicios__item"><img class="logo" src="./images/school-teacher.png" alt="Diseño web">
+                <h3 class="title">Plataforma de Gestión Educativa</h3>
+                <p class="descricion">Todas las herramientas en un solo lugar para la gestión académica, disciplinaria y
+                    administrativa de su institución. También para empresas: creación y gestión de su propia universidad
+                    corporativa.</p></div>
+            <div class="servicios__item"><img class="logo" src="./images/pixeltrue-plan-1 1.png" alt="Diseño web">
+                <h3 class="title">Software ERP</h3>
+                <p class="descricion">Administración en tiempo real del flujo financiero de su negocio. Controle
+                    eficazmente todos las operaciones y transacciones de su empresa.</p></div>
+            <div class="servicios__item"><img class="logo" src="./images/marginalia-679 1.png" alt="Diseño web">
+                <h3 class="title">Gestión de consultorios médicos</h3>
+                <p class="descricion">Control de consultorios, profesionales de la salud, historias clínicas, anamnesis,
+                    citas médicas, exámenes, tratamientos y muchos más. Facturación, control de ingresos y gastos.
+                    Gestión administrativa total.</p></div>
+        </div>
+    </div>
+</main>
+<section class="productos">
+    <div class="wraper"><h2>Nuestros productos</h2>
+        <nav class="productos__menuwraper">
+            <ul class="productos__menu">
+                <li class="menu__item activo">Diseño Web</li>
+                <li class="menu__item">Desarrollo de Apps</li>
+                <li class="menu__item">Desarrollo Web</li>
+                <li class="menu__item">Gestion Educativa</li>
+                <li class="menu__item">Gestion Empresarial</li>
+                <li class="menu__item">Gestion Salud</li>
+            </ul>
+        </nav>
+        <div class="productos__grid">
+            <div class="productos__item"><img class="logo" src="./images/personal_train.png" alt="Diseño web">
+                <h3 class="title">web entrenamiento personal</h3>
+                <p class="descricion">Rutinas de street workout</p></div>
+            <div class="productos__item"><img class="logo" src="./images/personal_train.png" alt="Diseño web">
+                <h3 class="title">web studio de sonido</h3>
+                <p class="descricion">Diseño minimalista</p></div>
+            <div class="productos__item"><img class="logo" src="./images/personal_train.png" alt="Diseño web">
+                <h3 class="title">web cinema</h3>
+                <p class="descricion">App para amigos</p></div>
+            <div class="productos__item"><img class="logo" src="./images/personal_train.png" alt="Diseño web">
+                <h3 class="title">web entrenamiento personal</h3>
+                <p class="descricion">Rutinas de street workout</p></div>
+            <div class="productos__item"><img class="logo" src="./images/personal_train.png" alt="Diseño web">
+                <h3 class="title">web studio de sonido</h3>
+                <p class="descricion">Diseño minimalista</p></div>
+            <div class="productos__item"><img class="logo" src="./images/personal_train.png" alt="Diseño web">
+                <h3 class="title">web cinema</h3>
+                <p class="descricion">App para amigos</p></div>
+        </div>
+    </div>
+</section>
+<section class="why">
+    <div class="wraper">
+        <div class="why__content"><h2 class="why__title">¿Por Qué Elegirnos?</h2>
+            <ul class="why__caracteristicas">
+                <li><i class="fas fa-code"></i>Software en la nube. No requiere licencias.</li>
+                <li><i class="fas fa-code"></i>Software e infraestructura como servicio. No debe comprar equipos
+                    especializados.
+                </li>
+                <li><i class="fas fa-code"></i>Módulos totalmente parametrizables y ajustables a las necesidades de cada
+                    negocio. Usted elige lo que realmente necesite.
+                </li>
+                <li><i class="fas fa-code"></i>No manejamos paquetes, solo aplicaciones individuales.</li>
+                <li><i class="fas fa-code"></i>Su negocio tendrá <strong>+Conocimientos</strong>,
+                    <strong>+Experiencia</strong>, <strong>+Habilidades</strong> y el respaldo de un equipo de
+                    profesionales a su servicio.
+                </li>
+                <li><i class="fas fa-code"></i>Precios increíbles.</li>
+            </ul>
+            <div class="why__video">
+                <video src="./videos/appsiel.mp4" preload="none" playsinline controls muted loop
+                       poster="./images/appsiel-poster.jpg" autoplay></video>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="our__team">
+    <div class="wraper">
+        <div class="our__team__content"><h2 class="our__team__title">Nuestro Equipo</h2>
+            <blockquote class="person"><img class="person__img" src="./images/jonny.jpg"
+                                            alt="Camilo Andres Colon Cañizares">
+                <div class="person__descripcion"><h3 class="person__nombre">Camilo Andres Colon Cañizares</h3>
+                    <p class="person__profesion">fullstack</p><cite>“vive aprendiendo la vida nunca para de enseñar
+                        ”</cite></div>
+            </blockquote>
+            <div class="peoples">
+                <div class="worker"><img class="worker__img" src="./images/Selmary.jpg" alt="Selmary Medina Pérez">
+                    <div class="worker__descripcion"><h3 class="worker__nombre">Selmary Medina Pérez</h3>
+                        <p class="worker__profesion">Community Manager</p></div>
+                </div>
+                <div class="worker"><img class="worker__img" src="./images/jonny.jpg"
+                                         alt="Camilo Andres Colon Cañizares">
+                    <div class="worker__descripcion"><h3 class="worker__nombre">Camilo Andres Colon Cañizares</h3>
+                        <p class="worker__profesion">fullstack</p></div>
+                </div>
+                <div class="worker"><img class="worker__img" src="./images/jordan.jpg"
+                                         alt="Jordan Jhonsson Cuadro Negrete">
+                    <div class="worker__descripcion"><h3 class="worker__nombre">Jordan Jhonsson Cuadro Negrete</h3>
+                        <p class="worker__profesion">Backend</p></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="newsletter">
+    <div class="wraper">
+        <div class="newsletter__content"><h2 class="newsletter__title">NEWSLETTER</h2>
+            <p class="newsletter__descripcion">Suscribete a nuestro newsletter y te haremos llegar nuestras promociones,
+                descuentos, productos y todo el contenido de tecnología que nuestro maravilloso equipo prepara cada
+                semana.
+            </p>
+            <form id="formulario" method="post" action="{{route('newsletter.store')}}">
+                @csrf
+                <div class="newsletter__suscribete"><input name="email" type="email" placeholder="user@example.com" required>
+                    <button type="button" onclick="guardar()">SUSCRÍBETE</button>
+                </div>
+            </form>
+
+        </div>
+    </div>
+</section>
+<section class="presupuesto">
+    <div class="wraper">
+        <div class="presupuesto_content">
+            <div class="presupuesto_descripcion"><h2>¿Te gustaría crear un proyecto con nosotros?</h2>
+                <p>Solicita presupuesto para tu web, app o desarrollo a medida. Te guiaremos para desarrollar tu idea
+                    con el sistema más eficiente según el código de programación que necesites.</p></div>
+            <a class="btn">SOLICITAR PRESUPUESTO</a></div>
+    </div>
+</section>
+<footer class="footer">
+    <div class="wraper">
+        <div class="footer__content">
+            <div class="footer__item"><img class="logo" src="./images/Lg_ 1.png" alt="">
+                <p>En APPSIEL, Desarrollamos y comercializamos aplicaciones web como servicio de software en la nube
+                    (SaaS) y brindamos acompañamiento para promover el crecimiento empresarial en nuestra región,
+                    nuestro país y el mundo entero.</p>
+                <h3>Nuestras Redes Sociales</h3>
+                <ul class="redes">
+                    <li><a href="" class="red"><i class="fab fa-facebook-f"></i></a></li>
+                    <li><a href="" class="red"><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="" class="red"><i class="fab fa-twitter"></i></a></li>
+                </ul>
+            </div>
+            <div class="footer__item"><h3 style="margin-bottom: 10px;">Contactanos</h3>
+                <ul>
+                    <li>Tel: 3017764758</li>
+                    <li>Correo: colonca1999@gmail.com</li>
+                    <li>Direccion: calle 18d # 36-125</li>
+                </ul>
+            </div>
+            <div class="footer__item"><h3>¿Donde Estamos?</h3>
+                <div class="mapa">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7070.497414606204!2d-73.24911022371593!3d10.531831415041987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e8ac78eb7683f0d%3A0x1fbc3c51106c318a!2s80%2C%20Valledupar%2C%20Cesar%2C%20Colombia!5e0!3m2!1ses-419!2sus!4v1590962883771!5m2!1ses-419!2sus"
+                        width="280" height="150" frameborder="0"
+                        style="border:0; border-radius: 20px; box-shadow: 0px 0px 10px 5px rgba(0,0,0,0.4);"
+                        tabindex="0" allowfullscreen="" aria-hidden="false"></iframe>
+                </div>
+            </div>
+        </div>
+        <p class="copyright">Copyright © 2020 Appsiel | Todos los derechos reservados·<a href="" class="politicas">Política
+                de Privacidad, Aviso Legal y Cookies</a>·</p></div>
+</footer>
+<script src="{{asset('main.bundle.js')}}"></script>
+<script src="{{asset('js/jquery.min.js')}}"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+<script>
+    window.onload = function () {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Disculpe las molestias...',
+            text: 'Nuestro Sitio Web actualmente está en mantenimiento, algunas de la funcionalidades aun no estan terminadas totalmente. !',
+        })
+    }
+
+    function guardar(){
+        var $request = $("#formulario").serialize();
+        $.post(
+            '{{url('newsletter/suscripcion/guardar/')}}', $request
+        ).done(function (msg){
+            if (msg.status == 'ok'){
+                Swal.fire({
+                    position: 'top-end',
+                    text:msg.response
+                });
+            }else{
+                Swal.fire({
+                    position: 'top-end',
+                    title: 'Ha surgido un problema',
+                    text: 'Error interno. inténtelo otra vez más tarde.'
+                });
+            }
+        })
+    }
+
+</script>
+</body>
+</html>
