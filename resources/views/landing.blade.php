7b408226e9ffc2db01b110562236cfaa2721d3d8
Adalberto Perez <adalberto.perez@appsiel.com.co>
2020-08-19 10:59:21 -0500

    landing page v2

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -7,6 +7,9 @@
 <meta name=viewport content="width=device-width, initial-scale=1.0">
 <meta name=format-detection content="telephone=no">
 <meta http-equiv=X-UA-Compatible content="IE=edge,chrome=1">
+
+<link rel="icon" type="image/png" href="{{asset('images/portada_ebook.ico')}}" />
+
 <meta name=description content="A continuación se mencionan cinco (5) estrategias claves que usted debe empezar a fortalecer para lograr sus objetivos de crecimiento continuo; para que su empresa o negocio sea sostenible en el tiempo y usted llegue a ser un empresario exitoso. Además se sugieren las herramientas necesarias para una exitosa implementa">
 <meta name=keywords content="crecimiento empresarial">
 <meta name=robots content=noindex,nofollow>
@@ -22,7 +25,7 @@
 <main>
     <section class="section section-relative" id=page-block-8ebj5wxjmle data-at=section>
         <div class=section-block>
-            <div class="section-inner section-fit section-relative">
+            <div class="section-inner section-fit section-relative" id="form_campania">
                 <div class="widget item-absolute headline" id=element-5 data-at=headline>
                     <div class=contents>
                         <h5>
@@ -42,9 +45,9 @@
                     <form novalidate class="form email-form contents form-labels-outside-top" method="post" data-id=element-3 data-at=form action="{{route('landing.store')}}">
                         @csrf
                         <label class="form-label-title form-label-outside form-label" for=field-3f8d0a04e94f734021951ca03ec8c0c3-0 data-at=form-text-title>Nombre</label>
-                        <input id=field-3f8d0a04e94f734021951ca03ec8c0c3-0 class="form-input form-input-text required" data-at=form-text type=text name="nombre" value title=Nombre placeholder data-label-inside=Nombre required>
-                        <label class="form-label-title form-label-outside form-label" for=field-3f8d0a04e94f734021951ca03ec8c0c3-1 data-at=form-email-title>Correo Electronico</label>
-                        <input id=field-3f8d0a04e94f734021951ca03ec8c0c3-1 class="form-input form-input-text required" type=email name="correo" value title="Correo Electronico" data-label-inside="Correo Electronico" data-at=form-email placeholder required>
+                        <input id=field-3f8d0a04e94f734021951ca03ec8c0c3-0 class="form-input form-input-text required" data-at=form-text type=text name="nombre" value title=Nombre placeholder data-label-inside=Nombre required="required">
+                        <label class="form-label-title form-label-outside form-label" for=field-3f8d0a04e94f734021951ca03ec8c0c3-1 data-at=form-email-title>Correo Electrónico</label>
+                        <input id=field-3f8d0a04e94f734021951ca03ec8c0c3-1 class="form-input form-input-text required" type=email name="correo" value title="Correo Electronico" data-label-inside="Correo Electronico" data-at=form-email placeholder required="required">
 
 
                         <div class="item-absolute form-btn-geometry">
@@ -60,7 +63,11 @@
                 <div class="widget item-absolute headline" id=element-9 data-at=headline>
                     <div class=contents>
                         <h1>
-                            <p class="x_c610ae00 x_b552f409"><p class="x_b6ee196c x_12f16b9a"> <p class="x_5f0ed501 x_12f16b9a"> <strong><span class=x_e33f2b34>Lo Quiero</span></strong><p class="x_b6ee196c x_12f16b9a"> </p>
+                            <p class="x_5f0ed501 x_12f16b9a"> 
+                            	<strong>
+                            		<span class=x_e33f2b34>Una guía eficaz</span>
+                            	</strong>
+                            </p>
                             @if(isset($message))
                                 <p class="x_5f0ed501 x_12f16b9a"> <strong><span class=x_e33f2b34>{{$message}}</span></strong></p>
                             @endif
@@ -69,7 +76,7 @@
                 </div>
                 <div class="widget item-absolute paragraph" id=element-10 data-at=paragraph>
                     <div class=contents>
-                        <p class=x_dc68832a>Adquiere un libro totalmente gratuito sobre el crecimiento empresarial</p>
+                        <p class=x_dc68832a>... y planes de acción para su implementación.</p>
                     </div>
                 </div>
                 <div class="widget item-absolute" id=element-99>
@@ -79,7 +86,7 @@
             </div>
         </div>
     </section>
-    
+
     <section class="section section-relative" id=page-block-6i8opcv8uwj data-at=section>
 
         <div class="section-holder-border item-block item-absolute" data-at=section-border></div>
@@ -102,13 +109,13 @@
                 </div>
                 <div class="widget item-absolute paragraph" id=element-64 data-at=paragraph>
                     <div class=contents>
-                        <p class="x_6592d9fc x_fae6260b"><span class=x_f2074b6c>Redescubra las habilidades que lo han llevado a la creación de su negocio y la busqueda de crecimiento continuo.</span></p>
+                        <p class="x_6592d9fc x_fae6260b"><span class=x_f2074b6c>Redescubra las habilidades que son la base crecimiento continuo.</span></p>
                     </div>
                 </div>
                 <div class="widget item-absolute headline" id=element-65 data-at=headline>
                     <div class=contents>
                         <h3>
-                            <p class="x_00098c5b x_efc646ab"><p class="x_da6ad2be x_70463731"> <p class="x_da6ad2be x_70463731"><span class=x_f2074b6c>MINDSET EMPRESARIAL</span><p class="x_da6ad2be x_70463731"> </p>
+                            <p class="x_00098c5b x_efc646ab"><p class="x_da6ad2be x_70463731"> <p class="x_da6ad2be x_70463731"><span class=x_f2074b6c>PENSAMIENTO EMPRESARIAL</span><p class="x_da6ad2be x_70463731"> </p>
                         </h3>
                     </div>
                 </div>
@@ -147,25 +154,25 @@
                 </div>
                 <div class="widget item-absolute paragraph" id=element-76 data-at=paragraph>
                     <div class=contents>
-                        <p class="x_ccd98165 x_bb1de03d"><span class=x_f2074b6c>En qué enfocar sus actividades para lograr un crecimiento sostenido. Hay que clasificar nuestras actividades diarias y asignación de responsabilidades con base en estas tres áreas y tener un plan estratégico para cada una de ellas.</span></p>
+                        <p class="x_ccd98165 x_bb1de03d"><span class=x_f2074b6c>En qué enfocar sus actividades para lograr un crecimiento sostenido.</span></p>
                     </div>
                 </div>
                 <div class="widget item-absolute headline" id=element-77 data-at=headline>
                     <div class=contents>
                         <h3>
-                            <p class="x_6f8d78f4 x_024f4d12"><p class="x_6f8d78f4 x_024f4d12"> <p class="x_6f8d78f4 x_024f4d12"><span class=x_f2074b6c>Los tres pilares de las empresas</span><p class="x_6f8d78f4 x_024f4d12"> </p>
+                            <span class=x_f2074b6c>LOS TRES PILARES DEL DESARROLLO EMPRESARIAL</span><p class="x_6f8d78f4 x_024f4d12"> </p>
                         </h3>
                     </div>
                 </div>
                 <div class="widget item-absolute paragraph" id=element-78 data-at=paragraph>
                     <div class=contents>
-                        <p class="x_6592d9fc x_fae6260b"><span class=x_f2074b6c>Cómo gestionar el combustible principal del sostenimiento de su negocio: EL DINERO.</span></p>
+                        <p class="x_6592d9fc x_fae6260b"><span class=x_f2074b6c>Gestión del DINERO cómo combustible principal del sostenimiento de su negocio.</span></p>
                     </div>
                 </div>
                 <div class="widget item-absolute headline" id=element-79 data-at=headline>
                     <div class=contents>
                         <h3>
-                            <p class="x_00098c5b x_fcd2d15f"><p class=x_da6ad2be> <p class=x_da6ad2be><span class=x_f2074b6c>El flujo de efectivo</span><p class=x_da6ad2be> </p>
+                            <span class=x_f2074b6c>EL FLUJO DE EFECTIVO</span>
                         </h3>
                     </div>
                 </div>
@@ -194,34 +201,57 @@
                 </div>
                 <div class="widget item-absolute paragraph" id=element-168 data-at=paragraph>
                     <div class=contents>
-                        <p class="x_ccd98165 x_6f7ed428"><span class=x_f2074b6c>Aunque sea una empresa de comercialización siempre tendrá que brindar un servicio: servicio post-venta, servicio al consumidor, servicio de capacitación, servicio de envío, etc.</span><p class="x_ccd98165 x_6f7ed428"><span class=x_f2074b6c><br></span></p>
+                        <p class="x_ccd98165 x_6f7ed428"><span class=x_f2074b6c>Conozca a fondo los actores que participan en sus operaciones comerciales.</span><p class="x_ccd98165 x_6f7ed428"><span class=x_f2074b6c><br></span></p>
                     </div>
                 </div>
                 <div class="widget item-absolute headline" id=element-169 data-at=headline>
                     <div class=contents>
                         <h3>
-                            <p class="x_6f8d78f4 x_024f4d12"><p class="x_6f8d78f4 x_024f4d12"> <p class="x_6f8d78f4 x_024f4d12"><span class=x_f2074b6c>Los tres pilares de las empresas</span><p class="x_6f8d78f4 x_024f4d12"> </p>
+                            <span class=x_f2074b6c>TODOS BRINDAMOS SERVICIOS</span><p class="x_6f8d78f4 x_024f4d12"> </p>
                         </h3>
                     </div>
                 </div>
+
                 <div class="widget item-absolute paragraph" id=element-170 data-at=paragraph>
                     <div class=contents>
-                        <p class="x_6592d9fc x_35d6d26d"><span class=x_f2074b6c>¿Sabía usted que la Teoría General de los Sistemas fue desarrollada inicialmente en el campo de Biología?</span><p class="x_6592d9fc x_35d6d26d"><span class=x_f2074b6c><br></span></p>
+                        <span class=x_f2074b6c>Un enfoque más profundo que el solo uso de herramientas tecnológicas.</span>
                     </div>
                 </div>
+
                 <div class="widget item-absolute headline" id=element-171 data-at=headline>
                     <div class=contents>
                         <h3>
-                            <p class="x_00098c5b x_efc646ab"><p class="x_da6ad2be x_70463731"> <p class="x_da6ad2be x_70463731"><span class=x_f2074b6c>Sistematizar. Sistematizar. Sistematizar.</span><p class="x_da6ad2be x_70463731"> </p>
+                            <span class=x_f2074b6c>SISTEMATIZACIÓN</span>
                         </h3>
                     </div>
                 </div>
             </div>
         </div>
     </section>
+
+    <section>
+    	<div class=section-block>
+            	<div class=contents>
+
+            		<form novalidate class="form email-form contents form-labels-outside-top" data-at=form>
+
+            			<div class="item-absolute form-btn-geometry">
+                            <a type="submit" class="btn form-btn item-block" data-at="form-button" href="#form_campania" style="background: orange;">
+                                QUIERO EL LIBRO
+                            </a>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+    </section>
+	
+	<br><br>
+    
     <section class="section section-relative" data-at=section>
          <video src="{{asset('videos/appsiel.mp4')}}" style="width:100%;height: 450px;" preload="none" playsinline controls muted loop poster="{{asset('images/appsiel-poster.jpg')}}" autoplay></video>
     </section>
+
     <section class="section section-relative" id=page-block-fedr58tick4 data-at=section>
 
         <div class="section-holder-border item-block item-absolute" data-at=section-border></div>
@@ -230,12 +260,34 @@
             <div class="section-inner section-fit section-relative">
                 <div class="widget item-absolute paragraph" id=element-90 data-at=paragraph>
                     <div class=contents>
-                        <p class="x_b614627d x_47b745f5"><span class=x_f2074b6c><em>En APPSIEL S.A.S nos preocupamos por hacer crecer tu negocio con herramientas que te facilitan la vida.</em></span></p>
+                        <p class="x_b614627d x_47b745f5"><span class=x_f2074b6c><em>En APPSIEL S.A.S. nos preocupamos por hacer crecer tu negocio con herramientas que te facilitan la vida.</em></span></p>
                     </div>
                 </div>
             </div>
         </div>
     </section>
+
+    <br><br><br>
+
+    <section>
+    	<div class=section-block>
+            	<div class=contents>
+
+            		<form novalidate class="form email-form contents form-labels-outside-top" data-at=form>
+
+            			<div class="item-absolute form-btn-geometry">
+                            <a type="submit" class="btn form-btn item-block" data-at="form-button" href="#form_campania" style="background: orange;">
+                                QUIERO EL LIBRO
+                            </a>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+    </section>
+
+    <br><br><br>
+
     <section class="section section-relative" id=page-block-0o41ympp5pc9 data-at=section>
 
         <div class="section-holder-border item-block item-absolute" data-at=section-border></div>
@@ -244,7 +296,7 @@
             <div class="section-inner section-fit section-relative">
                 <div class="widget item-absolute paragraph" id=element-96 data-at=paragraph>
                     <div class=contents>
-                        <p class=x_938e8cfc><span class=x_f2074b6c>Copyright © 2020 APPSIEL. Todos los derechos reservados</span></p>
+                        <p class=x_938e8cfc><span class=x_f2074b6c>Copyright © 2020 APPSIEL S.A.S. Todos los derechos reservados</span></p>
                     </div>
                 </div>
             </div>
