74f34c9a2367b927c7d4338898a098fc210e9297
Adalberto Pérez Oliveros <ing.adalbertoperez@gmail.com>
2020-09-13 14:50:21 +0000

    Content

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -14,7 +14,7 @@
                 </li>
                 <li class="nav__item"><a class="nav__link" href="">Precios</a></li>
                 <li class="nav__item"><a class="nav__link" href="{{url('/blog')}}">Blog</a></li>
-                <li class="nav__item"><a class="nav__link" href="{{url('home/contactanos')}}">Contacto</a></li>
+                <li class="nav__item"><a class="nav__link" href="{{url('contactUs')}}">Contacto</a></li>
                 <li class="nav__item"><a class="nav__link -my-1 py-1 px-2 bg-blue-900 rounded-full" href="http://demo.appsiel.com.co/login" target="_blank">DEMO</a></li>
             </ul>
         </nav>
